f72a8a214bd92e31d65219bd6672e927d0e01155
ItzFlibat <72283993+ItzFlibat@users.noreply.github.com>
2024-12-13 18:04:42 -0600

    v0.3.1

---
+++ src/components/AdminRolesComponent.jsx
@@ -0,0 +1,54 @@
+const { TextElement, RolePill, DiscordConstants } =
+  require("../utils/modules").ModuleStore;
+const React = BdApi.React;
+
+const AdminRolesElement = ({ guild, settings, roles }) => {
+  if (!settings["showAdmin"]) return null;
+  if (settings["showAdmin"] == "channel") return null;
+
+  const adminRoles = [];
+  Object.values(roles).forEach((role) => {
+    if (
+      (role.permissions & BigInt(8)) == BigInt(8) &&
+      (settings["showAdmin"] == "include" ||
+        (settings["showAdmin"] == "exclude" && !role.tags?.bot_id))
+    ) {
+      adminRoles.push(role);
+    }
+  });
+
+  if (!adminRoles?.length) {
+    return null;
+  }
+
+  return (
+    <TextElement
+      color={TextElement.Colors.INTERACTIVE_NORMAL}
+      style={{
+        borderTop: "1px solid var(--background-tertiary)",
+        padding: 5,
+      }}
+    >
+      Admin roles:
+      <div
+        style={{
+          paddingTop: 5,
+        }}
+      >
+        {adminRoles.map((m) => (
+          <RolePill
+            key={m.id}
+            canRemove={false}
+            className={`shc-rolePill`}
+            disableBorderColor={true}
+            guildId={guild.id}
+            onRemove={DiscordConstants.NOOP}
+            role={m}
+          />
+        ))}
+      </div>
+    </TextElement>
+  );
+};
+
+export default React.memo(AdminRolesElement);
---
+++ src/components/Lockscreen.jsx
@@ -0,0 +1,180 @@
+const React = BdApi.React;
+
+import UserMentionsComponent from "./UserMentionsComponent";
+import ChannelRolesComponent from "./ChannelRolesComponent";
+import AdminRolesComponent from "./AdminRolesComponent";
+import ForumComponent from "./ForumComponent";
+import { convertToHMS, getDateFromSnowflake } from "../utils/date";
+
+const { TextElement, GuildStore, ChannelUtils } =
+  require("../utils/modules").ModuleStore;
+
+const CHANNEL_TYPES = {
+  0: "text",
+  2: "voice",
+  4: "category",
+  5: "news",
+  6: "store",
+  13: "stage",
+};
+
+export const Lockscreen = React.memo(({ chat, channel, settings }) => {
+  const guild = GuildStore.getGuild(channel.guild_id);
+  const guildRoles = GuildStore.getRoles(guild.id);
+
+  return (
+    <div
+      className={["shc-hidden-chat-content", chat].filter(Boolean).join(" ")}
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div className="shc-hidden-notice">
+        <img
+          style={{
+            WebkitUserDrag: "none",
+            maxHeight: 128,
+            margin: "0 auto",
+          }}
+          src={
+            settings["hiddenChannelIcon"] === "lock"
+              ? "https://discord.com/assets/433e3ec4319a9d11b0cbe39342614982.svg"
+              : "https://discord.com/assets/433e3ec4319a9d11b0cbe39342614982.svg"
+          }
+        />
+        <TextElement
+          color={TextElement.Colors.HEADER_PRIMARY}
+          size={TextElement.Sizes.SIZE_32}
+          style={{
+            marginTop: 20,
+            fontWeight: "bold",
+          }}
+        >
+          {`This is a hidden ${
+            CHANNEL_TYPES[channel.type] ?? "unknown"
+          } channel`}
+        </TextElement>
+        <TextElement
+          color={TextElement.Colors.HEADER_SECONDARY}
+          size={TextElement.Sizes.SIZE_16}
+          style={{
+            marginTop: 8,
+          }}
+        >
+          You cannot see the contents of this channel.{" "}
+          {channel.topic &&
+            channel.type != 15 &&
+            "However, you may see its topic."}
+        </TextElement>
+        {/* Topic */}
+        {channel.topic &&
+          channel.type != 15 &&
+          (ChannelUtils?.renderTopic(channel, guild) ||
+            "ChannelUtils module is missing, topic won't be shown.")}
+
+        {/* Icon Emoji */}
+        {channel?.iconEmoji && (
+          <TextElement
+            color={TextElement.Colors.INTERACTIVE_NORMAL}
+            size={TextElement.Sizes.SIZE_14}
+            style={{
+              marginTop: 16,
+            }}
+          >
+            Icon emoji: {channel.iconEmoji.name ?? channel.iconEmoji.id}
+          </TextElement>
+        )}
+
+        {/* Slowmode */}
+        {channel.rateLimitPerUser > 0 && (
+          <TextElement
+            color={TextElement.Colors.INTERACTIVE_NORMAL}
+            size={TextElement.Sizes.SIZE_14}
+          >
+            Slowmode: {convertToHMS(channel.rateLimitPerUser)}
+          </TextElement>
+        )}
+
+        {/* NSFW */}
+        {channel.nsfw && (
+          <TextElement
+            color={TextElement.Colors.INTERACTIVE_NORMAL}
+            size={TextElement.Sizes.SIZE_14}
+          >
+            Age-Restricted Channel (NSFW) 🔞
+          </TextElement>
+        )}
+
+        {/* Bitrate */}
+        {channel.bitrate && channel.type == 2 && (
+          <TextElement
+            color={TextElement.Colors.INTERACTIVE_NORMAL}
+            size={TextElement.Sizes.SIZE_14}
+          >
+            Bitrate: {channel.bitrate / 1000}kbps
+          </TextElement>
+        )}
+
+        {/* Creation date */}
+        <TextElement
+          color={TextElement.Colors.INTERACTIVE_NORMAL}
+          size={TextElement.Sizes.SIZE_14}
+          style={{
+            marginTop: 8,
+          }}
+        >
+          Created on: {getDateFromSnowflake(channel.id)}
+        </TextElement>
+
+        {/* Last message */}
+        {channel.lastMessageId && (
+          <TextElement
+            color={TextElement.Colors.INTERACTIVE_NORMAL}
+            size={TextElement.Sizes.SIZE_14}
+          >
+            Last message sent: {getDateFromSnowflake(channel.lastMessageId)}
+          </TextElement>
+        )}
+
+        {/* Permissions */}
+        {settings["showPerms"] && channel.permissionOverwrites && (
+          <div
+            style={{
+              margin: "16px auto 0 auto",
+              backgroundColor: "var(--background-secondary)",
+              padding: 10,
+              borderRadius: 5,
+              color: "var(--text-normal)",
+            }}
+          >
+            {/* Users */}
+            <UserMentionsComponent
+              channel={channel}
+              guild={guild}
+              settings={settings}
+            />
+
+            {/* Channel Roles */}
+            <ChannelRolesComponent
+              channel={channel}
+              guild={guild}
+              settings={settings}
+              roles={guildRoles}
+            />
+
+            {/* Admin Roles */}
+            <AdminRolesComponent
+              guild={guild}
+              settings={settings}
+              roles={guildRoles}
+            />
+          </div>
+        )}
+
+        {/* Forums */}
+        <ForumComponent channel={channel} />
+      </div>
+    </div>
+  );
+});
